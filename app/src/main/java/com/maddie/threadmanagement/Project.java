3440773d5a43f0c1a71c2b91d508e507597e031a
MagneticRegulus <ladymeckbert@gmail.com>
2021-01-05 15:25:29 +1300

    Add Thread & Project classes

---
+++ app/src/main/java/com/maddie/threadmanagement/Project.java
@@ -0,0 +1,105 @@
+package com.maddie.threadmanagement;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Project {
+
+    private String status;
+    private String title;
+    //private String resourceLink;
+    //private String notes;
+    private Set<Thread> threadList;
+    private static final String NOT_STARTED = "Not Started";
+    private static final String IN_PROGESS = "In Progress";
+    private static final String FINISHED = "Finished";
+
+    public Project(String title) {
+        this.status = NOT_STARTED;
+        this.title = title;
+        this.threadList = new HashSet<>();
+    }
+
+    public void toggleStarted() {
+        if (status.equals(NOT_STARTED)) {
+            status = IN_PROGESS;
+        } else if (status.equals(IN_PROGESS)) {
+            status = NOT_STARTED;
+        }
+    }
+
+    public void toggleFinished() {
+        if (status.equals(FINISHED)) {
+            status = IN_PROGESS;
+        } else if (status.equals(IN_PROGESS)) {
+            status = FINISHED;
+        }
+    }
+
+    public boolean addThread(Thread thread) {
+        if (threadList.contains(thread)) {
+            return false;
+        }
+        threadList.add(thread);
+        return true;
+    }
+
+    public boolean removeThread(Thread thread) {
+        if (threadList.contains(thread)) {
+            threadList.remove(thread);
+            return true;
+        }
+        return false;
+    }
+
+    //getters & setters
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Thread> getThreadList() {
+        return threadList;
+    }
+
+    public void setThreadList(Set<Thread> threadList) {
+        this.threadList = threadList;
+    }
+
+    //to string
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    //Equals & Hash
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Project project = (Project) o;
+        return title.equals(project.title);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+}
---
+++ app/src/main/java/com/maddie/threadmanagement/Thread.java
@@ -0,0 +1,162 @@
+package com.maddie.threadmanagement;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Thread {
+
+    private boolean inStock;
+    private boolean need;
+    private boolean lowStock;
+    private String dmc;
+    private String name;
+    private String hexColor;
+    //private String notes;
+    //private String storageId;
+    private int skeinQty;
+    private Set<Project> projects;
+
+    public Thread(String dmc, String name, String hexColor) {
+        this.dmc = dmc;
+        this.name = name;
+        this.hexColor = hexColor;
+        this.skeinQty = 0;
+        this.projects = new HashSet<>();
+        this.inStock = false;
+        this.need = false;
+        this.lowStock = false;
+    }
+
+    public void addToStock() {
+        setInStock(true);
+        increaseQty(); //should set to 1
+    }
+
+    public void removeFromStock() {
+        setInStock(false);
+        skeinQty = 0;
+    }
+
+    public void toggleStock() {
+        if (inStock) {
+            removeFromStock();
+        } else {
+            addToStock();
+        }
+    }
+
+    public void toggleLowStock() {
+        if (inStock) {
+            lowStock = !lowStock;
+        }
+    }
+
+    public void increaseQty() {
+        skeinQty++;
+    }
+
+    public void decreaseQty() {
+        skeinQty--;
+    }
+
+    public boolean addProject(Project project) {
+        if (projects.contains(project)) {
+            return false;
+        }
+        projects.add(project);
+        return true;
+    }
+
+    public boolean removeProject(Project project) {
+        if (projects.contains(project)) {
+            projects.remove(project);
+            return true;
+        }
+        return false;
+    }
+
+    //getters & setters
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    public boolean isNeed() {
+        return need;
+    }
+
+    public void setNeed(boolean need) {
+        this.need = need;
+    }
+
+    public boolean isLowStock() {
+        return lowStock;
+    }
+
+    public void setLowStock(boolean lowStock) {
+        this.lowStock = lowStock;
+    }
+
+    public String getDmc() {
+        return dmc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHexColor() {
+        return hexColor;
+    }
+
+    public int getSkeinQty() {
+        return skeinQty;
+    }
+
+    public void setSkeinQty(int skeinQty) {
+        this.skeinQty = skeinQty;
+    }
+
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
+    //String
+
+    @Override
+    public String toString() {
+        return "DMC: " + dmc +
+                " - " + name +
+                " QTY: " + skeinQty;
+    }
+
+    //equals and hashcode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Thread thread = (Thread) o;
+        return dmc.equals(thread.dmc) &&
+                name.equals(thread.name) &&
+                hexColor.equals(thread.hexColor);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(dmc, name, hexColor);
+    }
+}
